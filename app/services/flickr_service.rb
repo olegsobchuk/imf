a13fded5a3d6296dc92d3035f94fe98ff41e2aac
olegsochuk <oleg.sobchuk@gmail.com>
2016-06-25 22:26:30 +0300

    Add flickr service

---
+++ app/services/flickr_service.rb
@@ -0,0 +1,20 @@
+class FlickrService
+  class << self
+    def photos
+      @photos ||= flickr.photos  
+    end
+
+    def upload(user, image_path)
+      image_id = flickr.upload_photo image_path, title: user.id
+      flickr.groups.pools.add(group_id: SECRETS[:flickr_group], photo_id: image_id) if image_id
+    end
+
+    def remove(image_id)
+      photos.delete(photo_id: image_id)
+    end
+
+    def get_info(image_id)
+      photos.getInfo(photo_id: image_id)
+    end
+  end
+end
