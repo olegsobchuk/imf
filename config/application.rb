84d409496acd3af96eeb16c3c2678e00fbea7f10
olegsobchuk <oleg.sobchuk@gmail.com>
2016-06-27 18:05:44 +0300

    Add sidekiq

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ module Imf
   class Application < Rails::Application
     config.active_record.raise_in_transactional_callbacks = true
     config.active_record.belongs_to_required_by_default = true
+    config.active_job.queue_adapter = :sidekiq
   end
 end
 
