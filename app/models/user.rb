a27a0f3f35fa2e751caf6514f33d6d211d13f2ad
olegsochuk <oleg.sobchuk@gmail.com>
2016-06-28 18:32:21 +0300

    Implement WebSockets

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,11 @@
-class User < ActiveRecord::Base
+class User < ApplicationRecord
   has_secure_password
 
   validates :email, presence: true, format: { with: /.+@.+\..+/i }
   validates :password, length: { in: 6..30 }
 
-  has_one :profile
+  has_one :profile, dependent: :destroy
+  has_many :notifications
   has_and_belongs_to_many :companies
 
   after_create :create_profile
