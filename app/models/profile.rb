ca74b4cdf9d059edeaba96d450153dcd239c3a3b
olegsobchuk <oleg.sobchuk@gmail.com>
2016-07-04 11:08:25 +0300

    Upload avatar to flickr

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,19 @@
 class Profile < ApplicationRecord
+  attr_accessor :avatar
+
   belongs_to :user
+
+  validates :avatar, file_content_type: { allow: %r{^image\/.*} }
+
+  after_validation :upload_avatar
 
   def full_name
     "#{first_name} #{last_name}".strip
+  end
+
+  private
+
+  def upload_avatar
+    self.avatar_id = FlickrService.upload(user, avatar) if avatar && !avatar_id
   end
 end
